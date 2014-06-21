cfb539822996198f86e5927fbb6728ff8829114a
mbogdanoff <mbogdanov@mail.com>
2014-06-21 14:09:23 +0300

    Create generate_5.py

---
+++ generate_5.py
@@ -0,0 +1,21 @@
+import random
+
+orig = random.choice([41120, 45232, 49344, 53456, 57568,61680])
+insert = random.choice([10, 11, 12 ,13 ,14 ,15])
+number_1 = random.choice([7,8])
+
+if number_1 == 7:
+	number_2 = 5 
+elif number_1 == 8:
+	number_2 = 6
+
+a = orig|(insert<<number_1)
+b = orig|(insert<<number_2)
+
+answer = a^b;
+
+print ("0x%X")%orig
+print ("0x000%X")%insert
+print ("0x%X")%a
+print ("0x%X")%b
+print ("0x%X")%answer
